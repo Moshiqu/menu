02a261a6732edd1afa4ba331c4bf33eb821e9a22
Moshiqu <912323520@qq.com>
2024-05-13 23:27:59 +0800

    添加日历插件

---
+++ pages/order/components/Calender/index.vue
@@ -0,0 +1,16 @@
+<template>
+    <view>
+        <Calendar @change="change" />
+    </view>
+</template>
+
+<script setup>
+import Calendar from './components/calendar.vue'
+
+
+const change = () => {
+
+}
+</script>
+
+<style scoped lang="less"></style>
--- pages/order/index.vue
+++ pages/order/index.vue
@@ -1,13 +1,19 @@
 <template>
-    <div>
-
-    </div>
+    <view class="order_page">
+        <Calendar @change="change" />
+    </view>
 </template>
 
 <script setup>
+import Calendar from './components/Calender/calendar.vue'
 
+const change = () => {
+
+}
 </script>
 
 <style scoped lang="less">
-
+.order_page {
+    padding: 40rpx;
+}
 </style>
